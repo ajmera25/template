c79b3a3ce4aafb2be4e62afbe5f70c8aa3510231
Bharat <bharatslalchandani@gmail.com>
2019-09-15 11:52:55 +0530

    Adding method and updating test case flow

--- src/test/java/testcases/MaximumAttentionTweet.java
+++ src/test/java/testcases/MaximumAttentionTweet.java
@@ -1,13 +1,39 @@
 package testcases;
 
 import core.BaseTest;
+
+import java.util.HashMap;
+
 import org.testng.annotations.Test;
 import pageobjects.desktop.LoginPage;
+import pageobjects.desktop.SearchPage;
+import pageobjects.desktop.TweetPage;
+import utilities.TestFrameworkException;
 
 public class MaximumAttentionTweet extends BaseTest {
 
     @Test
-    public void getTestTweet(){
-      new LoginPage(driver).doLogin("SurnameTooLong","bharatdemo");
+    public void test001_getTestTweet() throws TestFrameworkException{
+      new LoginPage(driver).doLogin("SurnameTooLong", "bharatdemo");
+      new SearchPage(driver).searchTweet("@stepin_forum");
     }
+    
+    @Test
+    public void test002_selectTweet() throws TestFrameworkException{
+    	//open tweet with maximum attention
+    }
+    
+    @Test
+    public void test003_getInformationOfTweet() throws TestFrameworkException{
+    	HashMap<String, String> hMapInfoLiked = new HashMap<>();
+    	HashMap<String, String> hMapInfoReTweeted = new HashMap<>();
+    	hMapInfoLiked = new TweetPage(driver).getMapOfHandleAndInformationWhoLiked
+    			(new TweetPage(driver).getListOfHandleOfUsersWhoLiked());
+    	hMapInfoReTweeted = new TweetPage(driver).getMapOfHandleAndInformationWhoReTweeted
+    			(new TweetPage(driver).getListOfHandleOfUsersWhoRetweeted());
+    	
+    	System.out.println(hMapInfoLiked);
+    	System.out.println(hMapInfoReTweeted);
+    }
+    
 }
