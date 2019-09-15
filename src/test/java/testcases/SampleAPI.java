b3334595784957cad11169690712b4acf7bd9f37
Pratik Jain <jainpratik25@gmail.com>
2019-09-15 11:46:53 +0530

    description

--- src/test/java/testcases/SampleAPI.java
+++ src/test/java/testcases/SampleAPI.java
@@ -1,5 +1,11 @@
 package testcases;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import okhttp3.HttpUrl;
@@ -14,12 +20,15 @@ public class SampleAPI{
 	@Test
     public void getTestTweet() throws Exception{
 		APIUtils Utils = new APIUtils();
-		getRetweetersList(Utils.getMaxImpressionTweetId());
-
+		List<String> reTwitterList = getRetweetersList(Utils.getMaxImpressionTweetId());
+		for(String reTwitterId : reTwitterList) {
+			System.out.println(getFollowerFollowingCount(reTwitterId));
+		}
     }
 	
-	public void getRetweetersList(String tweetId) throws Exception {
+	public List<String> getRetweetersList(String tweetId) throws Exception {
 		//tweetId = "327473909412814850";
+		List<String> retwitterList;
 		OkHttpClient client = new OkHttpClient();
 
 		HttpUrl url = new HttpUrl.Builder().scheme("https").host("api.twitter.com")
@@ -29,7 +38,30 @@ public class SampleAPI{
 		Request request = new Request.Builder().url(url).build();
 
 		Response response = client.newCall(request).execute();
-		System.out.println(response.body().string());
+		retwitterList = Arrays.asList(response.body().string().replace("[", "").replace("]", "").replace("\"", "").split(","));
+		System.out.println(retwitterList);
+		return retwitterList;
+	}
+	
+	public List<String> getFollowerFollowingCount(String retweeterID) throws Exception {
+		List<String> userProfile = new ArrayList<>();
+		OkHttpClient client = new OkHttpClient();
+
+		HttpUrl url = new HttpUrl.Builder().scheme("https").host("api.twitter.com").addPathSegments("/1.1/users/lookup.json")
+				.addQueryParameter("user_id",retweeterID).build();
+
+			System.out.println(url);
+		Request request = new Request.Builder().url(url).build();
+		 Response response = client.newCall(request).execute();
+		 JSONArray array = new JSONArray(response.body().string());
+		 System.out.println(array);
+			 JSONObject object = array.getJSONObject(0);
+			 userProfile.add(object.get("followers_count").toString());
+			 userProfile.add(object.get("friends_count").toString());
+			userProfile.add(object.get("description").toString());
+			return userProfile;
+			 
+		
 	}
 	
 }
