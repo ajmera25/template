9bf6d8dd74edc516852e233abef155336874a9b5
Bharat <bharatslalchandani@gmail.com>
2019-09-15 11:14:29 +0530

    Adding tweet page and pageObjects

--- src/test/java/pageobjects/desktop/TweetPage.java
+++ src/test/java/pageobjects/desktop/TweetPage.java
@@ -3,8 +3,6 @@ package pageobjects.desktop;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
