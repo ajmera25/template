24b4ee08cb697c195d3e4ca26356451a1777b171
Pratik Jain <jainpratik25@gmail.com>
2019-09-19 16:02:00 +0530

    imdb page

--- src/test/java/pageobjects/desktop/IMDBSearchPage.java
+++ src/test/java/pageobjects/desktop/IMDBSearchPage.java
@@ -1,6 +1,8 @@
 package pageobjects.desktop;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import core.BasePage;
 
@@ -10,6 +12,9 @@ public class IMDBSearchPage extends BasePage{
 		super(driver);
 	}
 
+	@FindBy(name="q")
+	private WebElement searchMovieName;
+	
 	
 	public boolean setUrl(String url) throws Exception{
 		boolean bval = false;
@@ -21,5 +26,36 @@ public class IMDBSearchPage extends BasePage{
 		return bval;
 	}
 	
-
+	public boolean searchMovieName(String movieName) throws Exception{
+		boolean bval = false;
+		try{
+			pageWebDriverClient.setText(searchMovieName,movieName);
+			pageWebDriverClient.click("//div[@class='suggestionlabel']/span[@title='"+movieName+"']");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return bval;
+	}
+	
+	public boolean getImdbRating(String movieName) throws Exception{
+		boolean bval = false;
+		try{
+			pageWebDriverClient.setText(searchMovieName,movieName);
+			pageWebDriverClient.getText("//div[@class='imdbRating']//span");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return bval;
+	}
+	
+	public boolean getDirector(String movieName) throws Exception{
+		boolean bval = false;
+		try{
+			pageWebDriverClient.setText(searchMovieName,movieName);
+			pageWebDriverClient.getText(".//h4[text()='Director:']/following-sibling::a");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return bval;
+	}
 }
