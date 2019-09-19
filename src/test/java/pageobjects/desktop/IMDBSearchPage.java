21388d9b8923de26b5276a6666379035df1ca75a
Pratik Jain <jainpratik25@gmail.com>
2019-09-19 15:30:14 +0530

    updated

---
+++ src/test/java/pageobjects/desktop/IMDBSearchPage.java
@@ -0,0 +1,35 @@
+package pageobjects.desktop;
+
+import org.openqa.selenium.WebDriver;
+
+import core.BasePage;
+
+public class IMDBSearchPage extends BasePage{
+
+	public IMDBSearchPage(WebDriver driver) {
+		super(driver);
+	}
+
+	
+	public boolean setUrl(String url) throws Exception{
+		boolean bval = false;
+		try{
+			pageWebDriverClient.setURL(url);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return bval;
+	}
+	
+	public boolean setUrl(String url) throws Exception{
+		boolean bval = false;
+		try{
+			pageWebDriverClient.setURL(url);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return bval;
+	}
+	
+
+}
--- src/test/java/testcases/GetFavMovie.java
+++ src/test/java/testcases/GetFavMovie.java
@@ -10,24 +10,34 @@ import org.testng.annotations.Test;
 
 import core.DriverManagerFactory;
 import io.appium.java_client.AppiumDriver;
-import workflow.Workflow;
 
 public class GetFavMovie{
+	DriverManagerFactory dmf;
+	
+	/*@BeforeSuite
+	public void beforeSuite() {
+		dmf = new DriverManagerFactory();
+		try {
+			String platform = System.getProperty("platform");
+			dmf.initializeDriver(platform);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}*/
 	
 	@Test
-	public void method1() throws Exception {
-		Workflow wf = new Workflow();
+	public void favMovies() throws Exception {
+		DriverManagerFactory dmf = new DriverManagerFactory();
 		Constructor<?> constructor;
-		DriverManagerFactory DMF = new DriverManagerFactory();
 		String platform = System.getProperty("platform");
-		DMF.initializeDriver(platform);
+		dmf.initializeDriver(platform);
 		Class<?> clazz = Class.forName("pageobjects" + "." + platform  + "." + "YoutubeHomePage");
 		if(platform.equalsIgnoreCase("Desktop")) {
 			constructor = clazz.getConstructor(WebDriver.class);
 		}else {
 			constructor = clazz.getConstructor(AppiumDriver.class);
 		}
-		Object c = constructor.newInstance(DMF.getDriver(platform));
+		Object c = constructor.newInstance(dmf.getDriver(platform));
 		//String parameter
 		Class[] paramString = new Class[1];	
 		paramString[0] = String.class;
@@ -38,13 +48,12 @@ public class GetFavMovie{
 		Method clickLatestMovie = clazz.getDeclaredMethod("clickMovies");
 		clickLatestMovie.invoke(c).toString();
 		
-		Method getMovieList = clazz.getDeclaredMethod("getMovieList");
+		Method getMovieList = clazz.getDeclaredMethod("getMoviesList");
 		getMovieList.invoke(c).toString();
 	}
 	
 	@AfterSuite
 	public void afterSuite() throws Exception {
-		DriverManagerFactory dmf = new DriverManagerFactory();
 	 	try{
 	 		if(dmf.getDesktopWebDriver()!=null)
 	 		dmf.getDesktopWebDriver().quit();
