5bc82f639e014b260dfb5b55cf07c14949ba3986
Sachin Kadukar <sachink1586@gmail.com>
2019-09-19 15:36:53 +0530

    Added methods

--- src/test/java/pageobjects/desktop/YoutubeHomePage.java
+++ src/test/java/pageobjects/desktop/YoutubeHomePage.java
@@ -1,24 +1,34 @@
 package pageobjects.desktop;
 
+import java.util.HashMap;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 import core.BasePage;
-import utilities.TestFrameworkException;
 
 public class YoutubeHomePage extends BasePage{
 
 	public YoutubeHomePage(WebDriver driver) {
 		super(driver);
 	}
+	
+	public void setUrl(String url) throws Exception{
+		try{
+			pageWebDriverClient.setURL(url);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 
 	public boolean clickMovies() throws Exception{
 		boolean bval = false;
 		try{
 			String moviesLink = "//span[text()='Movies']";
+			String viewAllLink = "//paper-button[@aria-label='View all']";
 			pageWebDriverClient.waitForElementToBeClickable(moviesLink);
 			pageWebDriverClient.click(moviesLink);
+			pageWebDriverClient.waitForElementToBeClickable(viewAllLink);
+			pageWebDriverClient.click(viewAllLink);
 			bval = pageWebDriverClient.waitForVisibilityThenCheckIsWebElementDisplayed("//span[text()='New Releases']");
 		}catch(Exception e){
 			e.printStackTrace();
@@ -26,14 +36,21 @@ public class YoutubeHomePage extends BasePage{
 		return bval;
 	}
 	
-	public boolean getMoviesList () {
-		boolean bval = false;
+	public HashMap<String, String> getMoviesList () {
+		HashMap<String, String> moviesList = new HashMap<String, String>();
+		String movieTitle = "//span[@id='video-title']";
+		String movePrice = "//span[contains(text(),'From')]";
+		String movie = "//ytd-grid-movie-renderer";
 		try{
-			String moviesTitle = "//span[@id='video-title']";
+			
+			for (int i=1; i<=20; i++) {
+				moviesList.put(pageWebDriverClient.getText(movie+"["+i+"]"+movieTitle),pageWebDriverClient.getText(movie+"["+i+"]"+movePrice));
+			}
+			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		return bval;
+		return moviesList;
 	}
 
 }
