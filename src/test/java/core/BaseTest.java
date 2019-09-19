7ae74c6f674067e3ef36d35941478b3cef9d62d7
Sachin Kadukar <sachink1586@gmail.com>
2019-09-19 14:56:19 +0530

    Initial commit

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -1,28 +1,39 @@
 package core;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.Reporter;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
-
+	
+	public WebDriver driver;
+	
 	@BeforeSuite
 	public void beforeSuite() {
-		
+		DriverManagerFactory DMF = new DriverManagerFactory();
+		try {
+			DMF.initializeDriver("desktop");
+			driver = DMF.getDesktopWebDriver();
+			driver.get("https://youtube.com");
+			driver.manage().window().maximize();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@AfterSuite
-		public void afterSuite() throws Exception {
-			DriverManagerFactory dmf = new DriverManagerFactory();
-		 	try{
-		 		if(dmf.getDesktopWebDriver()!=null)
-		 		dmf.getDesktopWebDriver().quit();
-		 		if(dmf.getAppiumDriver()!=null)
-		 		dmf.getAppiumDriver().quit();
-		        
-			}catch(Exception e){
-				Reporter.log("Error in Initializing the test", true);
-				e.printStackTrace();
-			}
+	public void afterSuite() throws Exception {
+		DriverManagerFactory dmf = new DriverManagerFactory();
+	 	try{
+	 		if(dmf.getDesktopWebDriver()!=null)
+	 		dmf.getDesktopWebDriver().quit();
+	 		if(dmf.getAppiumDriver()!=null)
+	 		dmf.getAppiumDriver().quit();
+	        
+		}catch(Exception e){
+			Reporter.log("Error in Initializing the test", true);
+			e.printStackTrace();
+		}
 	}
 }
